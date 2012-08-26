4e796b8e54c1179941025cf2338ac51a86fa2b11
Tatsh <audvare@gmail.com>
2012-08-25 19:31:28 -0700

    Enable debug logging

--- classes/kCore.php
+++ classes/kCore.php
@@ -1,7 +1,20 @@
 <?php
 class kCore extends sCore {
-  private static $settings = NULL;
+  const debugCallback            = 'kCore::debugCallback';
+  const exceptionClosingCallback = 'kCore::exceptionClosingCallback';
+  const getCache                 = 'kCore::getCache';
+  const getDatabase              = 'kCore::getDatabase';
+  const getSetting               = 'kCore::getSetting';
+  const isProductionMode         = 'kCore::isProductionMode';
   
+  /**
+   * @var array
+   */
+  private static $settings = NULL;
+
+  /**
+   * @return sCache
+   */
   public static function getCache() {
     if (self::$cache === NULL) {
       self::$cache = new sCache('apc');
@@ -9,6 +22,9 @@ class kCore extends sCore {
     return self::$cache;
   }
 
+  /**
+   * @return fDatabase
+   */
   public static function getDatabase() {
     if (self::$db === NULL) {
       $ini = parse_ini_file('./config/database.ini', TRUE);
@@ -40,6 +56,9 @@ class kCore extends sCore {
     return self::$db;
   }
 
+  /**
+   * @return mixed
+   */
   private static function cast($value, $type = NULL) {
     switch ($type) {
       case 'int':
@@ -63,14 +82,25 @@ class kCore extends sCore {
     return $value;
   }
 
+  /**
+   * @return mixed
+   */
   public static function getSetting($name, $cast_to = NULL, $default_value = NULL) {
+    $cache = self::getCache();
+    
     if (self::$settings === NULL) {
+      self::$settings = $cache->get('settings');
+    }
+
+    if (!is_array(self::$settings)) {
       $set = fRecordSet::build('SiteSetting');
       self::$settings = array();
-      
+
       foreach ($set as $item) {
         self::$settings[$item->getName()] = $item->getValue();
       }
+
+      $cache->set('settings', self::$settings, 7200);
     }
 
     if (isset(self::$settings[$name])) {
@@ -78,6 +108,13 @@ class kCore extends sCore {
     }
 
     return $default_value;
+  }
+
+  /**
+   * @return boolean
+   */
+  public static function isProductionMode() {
+    return self::getSetting('site.mode', 'boolean', FALSE);
   }
 
   public static function setSetting($name, $value) {
@@ -91,6 +128,7 @@ class kCore extends sCore {
     }
 
     $setting->store();
+
     self::$settings[$name] = $value;
   }
 
@@ -103,8 +141,29 @@ class kCore extends sCore {
     sAuthorization::setLoginPage('/login/');
   }
 
-  public static function isProductionMode() {
-    return self::getSetting('site.mode', 'boolean', FALSE);
+  /**
+   * Debug callback.
+   *
+   * @internal
+   *
+   * @param string $message Message to log.
+   * @return void
+   */
+  public static function debugCallback($message) {
+    $log_path = self::getSetting('site.error-log-destination', 'string', '/var/log/sutra/kusaba-y.log');
+
+    if (is_file($log_path)) {
+      file_put_contents($log_path, $message."\n", LOCK_EX | FILE_APPEND);
+    }
+  }
+
+  /**
+   * @internal
+   */
+  public static function exceptionClosingCallback() {
+    sResponse::sendServerErrorHeader();
+    fJSON::sendHeader();
+    print fJSON::encode('Unknown error occurred while generating this page.');
   }
 
   public static function configureTemplate() {
@@ -117,7 +176,15 @@ class kCore extends sCore {
 
   public static function main() {
     parent::main();
+    
+    $log_path = self::getSetting('site.error-log-destination', 'string', '/var/log/sutra/kusaba-y.log');
+
+    self::enableErrorHandling($log_path);
+    self::enableExceptionHandling($log_path, self::exceptionClosingCallback);
+    self::registerDebugCallback(self::debugCallback);
+    self::enableDebugging(!self::isProductionMode());
     self::configureTemplate();
+    
     kRouter::run();
   }
 }
