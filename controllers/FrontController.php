e79b076606a54a4476a89351a70a10d597b4e1c9
Andrew Udvare <audvare@gmail.com>
2012-11-01 01:37:38 -0700

    Use constant

--- controllers/FrontController.php
+++ controllers/FrontController.php
@@ -1,7 +1,7 @@
 <?php
 class FrontController extends MoorActionController {
   public function index() {
-    $categories = fRecordSet::build('Category', array(), array('name' => 'ASC'));
+    $categories = fRecordSet::build(Category, array(), array('name' => 'ASC'));
 
     $content = sTemplate::buffer('category-list-front', array('categories' => $categories));
     sTemplate::render(array('content' => $content, 'title' => ''));
